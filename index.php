ad3b4a3ee77fbe72c3ed4488821376dc52479ba8
Federica <federica.manograssi@gmail.com>
2021-05-10 14:42:22 +0200

    definizione metodo + 2 istanze

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
+ini_set('display_errors', 1);
 
 class Movie {
-    public $title;
+    private $title;
     public $director;
     public $genre;
-    public $year;
+    private $year;
+    private $age;
 
-    public function __construct($_title)
+    public function setAge(){
+        $this->age = getdate()['year'] - $this->year;
+    }
+
+    public function __construct($_title, $_year)
     {
         $this->title = $_title;
+        $this->year = $_year;
     }
 }
 
-$movie01 = new Movie('I Simpson - il film');
-
+$movie01 = new Movie('I Simpson - il film', 2007);
+$movie01->director = 'David Silverman';
+$movie01->genre = 'animazione';
+$movie01->setAge();
 var_dump($movie01);
 
-$movie02 = new Movie('Giallo in casa Muppet');
-
+$movie02 = new Movie('Giallo in casa Muppet',1981);
+$movie02->director = 'Jim Henson';
+$movie02->genre = 'animazione';
+$movie02->setAge();
 var_dump($movie02);
