5b3dc0ef4c00b0152d411006da4b37670c465934
Federica <federica.manograssi@gmail.com>
2021-05-10 14:52:59 +0200

    CONSEGNA
    stampa dei dati

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ class Movie {
     public $director;
     public $genre;
     private $year;
-    private $age;
+    public $age;
 
     public function setAge(){
         $this->age = getdate()['year'] - $this->year;
+    }
+
+    public function getTitle(){
+        return $this->title;
     }
 
     public function __construct($_title, $_year)
@@ -23,10 +27,15 @@ $movie01 = new Movie('I Simpson - il film', 2007);
 $movie01->director = 'David Silverman';
 $movie01->genre = 'animazione';
 $movie01->setAge();
-var_dump($movie01);
+
+echo "Titolo film : ".$movie01->getTitle()."<br>Regista : ".$movie01->director."<br>Genere : ".$movie01->genre."<br>Questo film ha ".$movie01->age." anni.<br>";
+
+// var_dump($movie01);
 
 $movie02 = new Movie('Giallo in casa Muppet',1981);
 $movie02->director = 'Jim Henson';
 $movie02->genre = 'animazione';
 $movie02->setAge();
-var_dump($movie02);
+// var_dump($movie02);
+
+echo "Titolo film : ".$movie02->getTitle()."<br>Regista : ".$movie02->director."<br>Genere : ".$movie02->genre."<br>Questo film ha ".$movie02->age." anni.<br>";
